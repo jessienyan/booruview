0b7551de60029c7074f8a12b2582d1c69658192a
jessienyan <jessienyan@noreply.codeberg.org>
2025-05-06 16:44:15 -0400

    Switch to flexbox, manage columns manually

--- client/src/components/PostContainer.vue
+++ client/src/components/PostContainer.vue
@@ -1,29 +1,19 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, useTemplateRef, watchEffect, watchPostEffect } from "vue";
+import { computed, onMounted, ref, useTemplateRef } from "vue";
 import PostContent from "./PostContent.vue";
 
 const { posts } = defineProps<{ posts: Post[] }>();
 const container = useTemplateRef("container");
 const columnCount = ref(0);
 
-const containerStyle = computed(() => {
-    if (!container.value) {
-        return null;
-    }
-    const style = getComputedStyle(container.value);
-    return {
-        colWidth: parseInt(style.columnWidth),
-        colGap: parseInt(style.columnGap),
-    };
-});
-
 function onResize() {
-    if (!container.value || !containerStyle.value) {
+    if (!container.value) {
         return;
     }
 
     let width = container.value.clientWidth;
-    const { colWidth, colGap } = containerStyle.value;
+    const colWidth = 600;
+    const colGap = 10;
     const colWithGap = colWidth + colGap;
 
     // prettier-ignore
@@ -36,8 +26,32 @@ function onResize() {
             );
 }
 
-watchEffect(() => {
-    // TODO: reorder columns
+const orderedPosts = computed(() => {
+    if(columnCount.value <= 1) {
+        return [posts];
+    }
+
+    let ordered: Post[][] = [];
+    let colHeight: number[] = [];
+    for(let i = 0; i < columnCount.value; i++) {
+        ordered = ordered.concat([[]]);
+        colHeight = colHeight.concat(0);
+    }
+
+    for(const j in posts) {
+        let shortestCol = 0;
+
+        for(let i = 1; i < columnCount.value; i++) {
+            if(colHeight[i] < colHeight[shortestCol]) {
+                shortestCol = i;
+            }
+        }
+
+        ordered[shortestCol] = ordered[shortestCol].concat(posts[j]);
+        colHeight[shortestCol] += posts[j].height / posts[j].width;
+    }
+
+    return ordered;
 })
 
 onMounted(() => {
@@ -47,13 +61,26 @@ onMounted(() => {
 
 <template>
     <div class="post-container" ref="container">
-        <PostContent :post="p" v-for="p in posts" :key="p.id" />
+        <div class="post-column" v-for="col, i in orderedPosts" :key="i">
+            <PostContent :post="post" v-for="post in col" :key="post.id" />
+        </div>
     </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .post-container {
-    column-width: 600px;
-    column-gap: 10px;
+    display: flex;
+    flex-direction: row;
+}
+
+.post-column {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    margin-right: 10px;
+
+    &:last-of-type {
+        margin-right: 0;
+    }
 }
 </style>
