62e60b23d00c0b8c31ef69bc7f6a4e04356cc2c5
jessienyan <jessienyan@noreply.codeberg.org>
2025-05-13 17:14:05 -0400

    Fix prod images not being tagged as latest

--- prod.sh
+++ prod.sh
@@ -7,10 +7,10 @@ COMMIT=$(git rev-parse --short master)
 DATE=$(date +%Y.%m.%d)
 TAG=$COMMIT-$DATE
 
-API_IMG=$PREFIX/booruview-api:$TAG
-CADDY_IMG=$PREFIX/booruview-caddy:$TAG
-CLIENT_IMG=$PREFIX/booruview-client:$TAG
-VALKEY_IMG=$PREFIX/booruview-valkey:$TAG
+API_IMG=$PREFIX/booruview-api
+CADDY_IMG=$PREFIX/booruview-caddy
+CLIENT_IMG=$PREFIX/booruview-client
+VALKEY_IMG=$PREFIX/booruview-valkey
 
 docker build -t $API_IMG -f api/Dockerfile.prod api/
 docker build -t $VALKEY_IMG valkey/
@@ -19,6 +19,15 @@ docker build -t $CLIENT_IMG client/
 OWNER=$(id -u):$(id -g)
 docker run --rm -e OWNER=$OWNER -v ./client/dist:/app/dist $CLIENT_IMG ash -c 'yarn build && chown -R $OWNER dist/'
 docker build -t $CADDY_IMG -f caddy/Dockerfile .
+
+docker tag $API_IMG $API_IMG:$TAG
+docker tag $API_IMG $API_IMG:latest
+
+docker tag $CADDY_IMG $CADDY_IMG:$TAG
+docker tag $CADDY_IMG $CADDY_IMG:latest
+
+docker tag $VALKEY_IMG $VALKEY_IMG:$TAG
+docker tag $VALKEY_IMG $VALKEY_IMG:latest
 
 docker push $API_IMG
 docker push $CADDY_IMG
