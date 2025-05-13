c36511ed95dd39b83459599a0ef75db808817125
jessienyan <jessienyan@noreply.codeberg.org>
2025-05-13 16:54:23 -0400

    Tag prod images with commit and date

--- prod.sh
+++ prod.sh
@@ -3,15 +3,18 @@
 set -e
 cd ${0%/*}
 PREFIX=ghcr.io/kangaroux
+COMMIT=$(git rev-parse --short master)
+DATE=$(date +%Y.%m.%d)
+TAG=$COMMIT-$DATE
 
-docker build -t $PREFIX/booruview-api -f api/Dockerfile.prod api/
-docker build -t $PREFIX/booruview-valkey valkey/
+docker build -t $PREFIX/booruview-api:$TAG -f api/Dockerfile.prod api/
+docker build -t $PREFIX/booruview-valkey:$TAG valkey/
 docker build -t $PREFIX/booruview-client client/
 
 OWNER=$(id -u):$(id -g)
 docker run --rm -e OWNER=$OWNER -v ./client/dist:/app/dist booruview-client ash -c 'yarn build && chown -R $OWNER dist/'
-docker build -t $PREFIX/booruview-caddy -f caddy/Dockerfile .
+docker build -t $PREFIX/booruview-caddy:$TAG -f caddy/Dockerfile .
 
-docker push $PREFIX/booruview-api
-docker push $PREFIX/booruview-caddy
-docker push $PREFIX/booruview-valkey
+docker push $PREFIX/booruview-api:$TAG
+docker push $PREFIX/booruview-caddy:$TAG
+docker push $PREFIX/booruview-valkey:$TAG
