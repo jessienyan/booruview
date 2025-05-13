628a363b3744e18b72c99cb693f20bb5d9a4cbef
jessienyan <jessienyan@noreply.codeberg.org>
2025-05-13 16:01:03 -0400

    Fix api Dockerfile, add production compose

--- prod.sh
+++ prod.sh
@@ -2,15 +2,16 @@
 
 set -e
 cd ${0%/*}
+PREFIX=ghcr.io/kangaroux
 
-echo "Building for production"
-
-docker build -t booruview-api -f api/Dockerfile.prod api/
-docker build -t booruview-valkey valkey/
-docker build -t booruview-client client/
+docker build -t $PREFIX/booruview-api -f api/Dockerfile.prod api/
+docker build -t $PREFIX/booruview-valkey valkey/
+docker build -t $PREFIX/booruview-client client/
 
 OWNER=$(id -u):$(id -g)
 docker run --rm -e OWNER=$OWNER -v ./client/dist:/app/dist booruview-client ash -c 'yarn build && chown -R $OWNER dist/'
-docker build -t booruview-caddy -f caddy/Dockerfile .
+docker build -t $PREFIX/booruview-caddy -f caddy/Dockerfile .
 
-echo "Done."
+docker push $PREFIX/booruview-api
+docker push $PREFIX/booruview-caddy
+docker push $PREFIX/booruview-valkey
