ac4239406cde71f8cd23c78b16a2fc943926ea8a
jessienyan <jessienyan@noreply.codeberg.org>
2025-05-29 19:56:31 -0400

    Start changing sidebar toggle to be a standard hamburg menu

--- client/src/components/sidebar/Sidebar.vue
+++ client/src/components/sidebar/Sidebar.vue
@@ -43,8 +43,7 @@ function onTagSelect(tag: Tag, negated: boolean) {
 <template>
     <header class="sidebar-container">
         <button class="toggle-btn" @click="$emit('toggle')">
-            <i v-if="closed" class="bi bi-chevron-right"></i>
-            <i v-else class="bi bi-chevron-left"></i>
+            <i class="bi bi-list"></i>
         </button>
         <div class="sidebar-content" v-show="!closed">
             <div class="search">
@@ -75,6 +74,7 @@ function onTagSelect(tag: Tag, negated: boolean) {
 
 .sidebar-container {
     position: relative;
+    background-color: $sidebar-bg-color;
 
     @media (max-width: $mobile-width) {
         .sidebar-open & {
@@ -84,7 +84,6 @@ function onTagSelect(tag: Tag, negated: boolean) {
 }
 
 .sidebar-content {
-    background-color: darken($bg-color, 2.5%);
     width: 450px;
     height: 100%;
     position: relative;
@@ -99,7 +98,7 @@ function onTagSelect(tag: Tag, negated: boolean) {
 }
 
 .search {
-    padding: 10px;
+    padding: 0 10px;
     min-height: 0;
     flex: 1;
     display: flex;
@@ -111,44 +110,11 @@ function onTagSelect(tag: Tag, negated: boolean) {
 }
 
 .toggle-btn {
-    $btn-color: #342b3a;
-    $border-color: lighten($btn-color, 20%);
-    $btn-height: 70px;
-
-    background-color: $btn-color;
-    border: 1px solid $border-color;
+    background: none;
+    border: none;
     color: #bb9fce;
-
-    font-size: 24px;
-    height: $btn-height;
-    padding: 0 8px;
-    border-left: none;
-    border-radius: 0 4px 4px 0;
-
-    position: absolute;
-    left: 100%;
-    top: calc(50% - $btn-height / 2);
-    z-index: 1;
+    font-size: 36px;
 
     cursor: pointer;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
-
-    .sidebar-closed & {
-        opacity: 0.5;
-
-        // Don't change opacity on mobile (fixes button getting stuck in hover state)
-        @media (pointer: fine) {
-            &:hover {
-                opacity: 1;
-            }
-        }
-    }
-
-    // Move the toggle btn to the bottom left side of the screen on mobile
-    @media (max-width: $mobile-width) {
-        .sidebar-open & {
-            left: 0;
-        }
-    }
 }
 </style>
