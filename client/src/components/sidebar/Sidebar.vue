f16130a7f42f452d403eecc6710c2c606b09ec0f
Jessie <2302541+Kangaroux@users.noreply.github.com>
2025-06-30 21:28:48 -0400

    Create new tab container in sidebar for search

--- client/src/components/sidebar/Sidebar.vue
+++ client/src/components/sidebar/Sidebar.vue
@@ -1,64 +1,9 @@
 <script setup lang="ts">
-import store from "@/store";
-import TagList from "../TagList.vue";
-import SearchForm from "../search/SearchForm.vue";
-import TabContainer from "./TabContainer.vue";
-import { computed } from "vue";
+import SidebarFooter from "./SidebarFooter.vue";
+import SidebarMain from "./SidebarMain.vue";
 
 defineEmits(["toggle"]);
-
 const { closed } = defineProps<{ closed: boolean }>();
-
-function doPostSearch() {
-    if (store.fetchingPosts) {
-        return;
-    }
-
-    store.currentPage = 1;
-    store.posts.clear();
-
-    store.searchPosts().catch(() => store.posts.clear());
-}
-
-function onTagSelect(tag: Tag, negated: boolean) {
-    if (!negated) {
-        store.query.includeTag(tag);
-    } else {
-        store.query.excludeTag(tag);
-    }
-
-    // Slight hack: try looking up unknown tags and replace it with the real version
-    if (tag.type === "unknown") {
-        store.loadTags([tag.name]).then(() => {
-            const real = store.cachedTags.get(tag.name);
-
-            if (real === undefined) {
-                return;
-            }
-
-            store.query.removeTag(tag);
-
-            if (!negated) {
-                store.query.includeTag(real);
-            } else {
-                store.query.excludeTag(real);
-            }
-        });
-    }
-}
-
-const styledTags = computed(() => {
-    let ret: TagChip[];
-
-    ret = store.query.includedList().map((tag) => ({ tag, style: "default" }));
-    ret = ret.concat(
-        store.query
-            .excludedList()
-            .map((tag) => ({ tag, style: "strikethrough" })),
-    );
-
-    return ret;
-});
 </script>
 
 <template>
@@ -70,19 +15,8 @@ const styledTags = computed(() => {
         </div>
 
         <div class="sidebar-content" v-show="!closed">
-            <div class="search">
-                <SearchForm
-                    @on-search="doPostSearch"
-                    @on-tag-select="onTagSelect"
-                    :show-spinner="store.fetchingPosts"
-                />
-
-                <div class="taglist-container">
-                    <TagList :jiggle="true" :tags="styledTags" />
-                </div>
-            </div>
-
-            <TabContainer />
+            <SidebarMain />
+            <SidebarFooter />
         </div>
     </header>
 </template>
@@ -122,19 +56,6 @@ const styledTags = computed(() => {
             width: 100%;
         }
     }
-}
-
-.search {
-    padding: 0 10px;
-    min-height: 0;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-}
-
-.taglist-container {
-    overflow-y: scroll;
-    flex: 1;
 }
 
 .toggle-btn {
