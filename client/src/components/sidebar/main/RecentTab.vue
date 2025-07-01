c38904d72cfb6d07365be240626856d902f85a52
Jessie <2302541+Kangaroux@users.noreply.github.com>
2025-07-01 14:36:06 -0400

    Add a delete button to recent search history

--- client/src/components/sidebar/main/RecentTab.vue
+++ client/src/components/sidebar/main/RecentTab.vue
@@ -22,30 +22,40 @@ function styledTags(query: SearchQuery) {
     });
 }
 
+function onDelete(index: number) {
+    store.settings.queryHistory.splice(index, 1);
+    store.settings.save();
+}
+
 function onSearch(query: SearchQuery) {
     store.query = query.copy();
     store.searchPosts();
-
-    console.log("search", query.asList());
 }
 </script>
 
 <template>
-    <div class="recent-list">
+    <div class="recent-list" ref="list">
         <div
             class="history-entry"
-            v-for="entry of store.settings.queryHistory"
+            v-for="(entry, i) of store.settings.queryHistory"
             :key="entry.date.getTime()"
         >
             <div class="tag-list">
                 <TagList :tags="styledTags(entry.query).value" />
             </div>
             <div class="entry-footer">
+                <button
+                    class="btn-delete"
+                    title="remove from history"
+                    @click="onDelete(i)"
+                >
+                    <i class="bi bi-trash3"></i>
+                </button>
                 <span class="time" :title="entry.date.toLocaleString()">{{
                     relativeTime(entry.date)
                 }}</span>
                 <button
-                    class="btn-primary btn-rounded"
+                    class="btn-primary btn-rounded btn-search"
                     @click="onSearch(entry.query)"
                 >
                     search
@@ -66,10 +76,8 @@ function onSearch(query: SearchQuery) {
 .entry-footer {
     border-top: 1px solid $color-primary-lighter;
     background-color: $color-primary-darker;
-    padding: 10px;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
     align-items: center;
 
     .time {
@@ -78,8 +86,22 @@ function onSearch(query: SearchQuery) {
     }
 }
 
+.btn-search {
+    margin: 10px 10px 10px auto;
+}
+
 .tag-list {
     padding: 10px;
     background-color: $color-primary-darker;
+}
+
+.btn-delete {
+    border: none;
+    background: none;
+    cursor: pointer;
+    padding: 10px;
+    margin: 0 5px;
+    font-size: 20px;
+    color: $color-primary-light;
 }
 </style>
