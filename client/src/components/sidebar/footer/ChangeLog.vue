d78fdab364fb8481ca328781440e42248d7ddab3
jessienyan <jessienyan@noreply.codeberg.org>
2025-07-09 14:14:37 -0400

    Tweak text color, fix changelog margin

--- client/src/components/sidebar/footer/ChangeLog.vue
+++ client/src/components/sidebar/footer/ChangeLog.vue
@@ -5,10 +5,8 @@ const { date, changes } = defineProps<ChangeLogEntry>();
 </script>
 
 <template>
-    <div class="entry">
-        <h5>{{ date }}</h5>
-        <ul>
-            <li v-for="change in changes" v-html="change"></li>
-        </ul>
-    </div>
+    <h3>{{ date }}</h3>
+    <ul>
+        <li v-for="change in changes" v-html="change"></li>
+    </ul>
 </template>
