c7c2979dbe93566e09fb5062dd59965a2726d700
jessienyan <jessienyan@noreply.codeberg.org>
2025-09-06 23:37:47 -0400

    Add a slide in animation for toasts so its more obvious when they appear

--- client/src/components/Toast.vue
+++ client/src/components/Toast.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import type store from "@/store";
+import { onMounted, ref } from "vue";
 
 type ToastKind = typeof store.toast.type;
 const { kind } = defineProps<{ kind: ToastKind }>();
-defineEmits(["dismiss"]);
+const emit = defineEmits(["dismiss"]);
+const dismissing = ref(false);
+const appearing = ref(true);
+
+function onDismiss() {
+    setTimeout(() => emit("dismiss"), 400);
+    dismissing.value = true;
+}
+
+onMounted(() => {
+    setTimeout(() => (appearing.value = false), 400);
+});
 </script>
 
 <template>
     <div
         class="toast"
-        :class="{ [`toast-${kind}`]: true }"
-        @click="$emit('dismiss')"
+        :class="{ [`toast-${kind}`]: true, appearing, dismissing }"
+        @click="onDismiss"
     >
         <slot></slot>
     </div>
@@ -18,6 +30,17 @@ defineEmits(["dismiss"]);
 
 <style lang="scss" scoped>
 @import "@/assets/colors";
+
+@keyframes slide-up {
+    0% {
+        bottom: 55px;
+        opacity: 0;
+    }
+    100% {
+        bottom: 80px;
+        opacity: 1;
+    }
+}
 
 .toast {
     position: absolute;
@@ -28,10 +51,21 @@ defineEmits(["dismiss"]);
     width: max-content;
     z-index: 999;
     border-radius: 0.8rem;
-    padding: 1rem 1.2rem;
+    padding: 1.2rem 1.6rem;
+    font-size: 1.1rem;
     cursor: pointer;
     filter: drop-shadow(0 0 10px black);
     color: #fff;
+    text-shadow: 0 0 3px #000a;
+}
+
+.appearing {
+    animation: 300ms ease-out slide-up;
+}
+
+.dismissing {
+    animation: 180ms ease-out reverse slide-up;
+    opacity: 0;
 }
 
 .toast-info {
