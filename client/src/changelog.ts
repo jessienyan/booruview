1f96ca9aab20addf20a14046fef222468c95c408
jessienyan <jessienyan@noreply.codeberg.org>
2025-09-19 23:52:42 -0400

    return to monke

--- client/src/changelog.ts
+++ client/src/changelog.ts
@@ -7,9 +7,8 @@ export const changeLog: ChangeLogEntry[] = [
     {
         date: "2025-09-19",
         changes: [
-            "setup cloudflare",
-            "configure cloudflare as a trusted proxy",
             "increase settings export size limit to 5MB",
+            "revert cloudflare migration, return to monke",
         ],
     },
     {
