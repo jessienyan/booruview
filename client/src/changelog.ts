969087fc7c95414cb68bb79a186d79a5523aa6aa
jessienyan <jessienyan@noreply.codeberg.org>
2025-12-06 18:02:26 -0500

    Update changelog

--- client/src/changelog.ts
+++ client/src/changelog.ts
@@ -5,6 +5,10 @@ export type ChangeLogEntry = {
 
 export const changeLog: ChangeLogEntry[] = [
 	{
+		date: "2025-12-06",
+		changes: ["added 'open in new tab' button when clicking on tags"],
+	},
+	{
 		date: "2025-12-02",
 		changes: ["changed hosting providers (linode -> hetzner)"],
 	},
