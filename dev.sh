17ee093a800f208de9e06c7f1e37644f9c34944d
jessienyan <jessienyan@noreply.codeberg.org>
2025-12-24 12:54:02 -0500

    Add sqlite db for storing users

--- dev.sh
+++ dev.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+cd ${0%/*}
+
+create_db() {
+    if [[ -e database/sqlite.db ]]; then
+        return
+    fi
+
+    docker run \
+        --rm \
+        -it \
+        -v "$(pwd)/database:/workspace" \
+        -w /workspace \
+        -u "$(id -u):$(id -g)" \
+        keinos/sqlite3 \
+        ash -c "sqlite3 sqlite.db < schema.sql"
+
+    echo "created db"
+}
 
 COMPOSE="docker compose"
 
@@ -10,5 +28,7 @@ fi
 
 export VITE_COMMIT_SHA=$(git rev-parse --short master)
 export VITE_LAST_COMMIT_DATE=$(git show -s --format=%cs master)
+
+create_db
 
 $COMPOSE up --build $@
