7671f081acf54cc5737da1a19d7b538f2b4d1aec
jessienyan <jessienyan@noreply.codeberg.org>
2026-01-18 19:51:30 -0500

    Add media proxy host to CSP

--- caddy/start.sh
+++ caddy/start.sh
@@ -3,4 +3,8 @@
 set -euo pipefail
 
 /etc/caddy/generate_caddyfile.sh
+
+MEDIA_PROXY_HOST_SCHEMELESS=${MEDIA_PROXY_HOST#http://}
+export MEDIA_PROXY_HOST_SCHEMELESS=${MEDIA_PROXY_HOST_SCHEMELESS#https://}
+
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
