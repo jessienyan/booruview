9f7115a4de497b87e64374e85ade5ebae0bbce5f
jessienyan <jessienyan@noreply.codeberg.org>
2026-01-25 23:25:20 -0500

    Add middleware for fetching user/data

--- api/routes/middleware.go
+++ api/routes/middleware.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
+	"strings"
 
+	api "codeberg.org/jessienyan/booruview"
+	"codeberg.org/jessienyan/booruview/models"
 	"github.com/rs/zerolog/log"
 )
 
@@ -30,6 +35,74 @@ func RecoverMiddleware(next http.Handler) http.Handler {
 				respondWithInternalError(w, fmt.Errorf("recovered from panic: %w", err))
 			}
 		}()
+
+		next.ServeHTTP(w, req)
+	})
+}
+
+type contextKey struct{}
+
+var userKey contextKey
+
+type userContextValue struct {
+	User models.Users
+	Data models.UserData
+}
+
+func getUser(req *http.Request) *userContextValue {
+	user := req.Context().Value(userKey)
+	if user == nil {
+		return nil
+	}
+	return user.(*userContextValue)
+}
+
+// If the auth token is valid, fetches the user and adds it to the request context.
+// If the token isn't valid, it does nothing.
+func LoadUserMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		userIDFromToken := func() (int64, bool) {
+			token := req.Header.Get("Authorization")
+			token = strings.TrimPrefix(token, "Bearer ")
+			if token == "" {
+				return 0, false
+			}
+
+			userID, err := api.ParseAuthToken(token)
+			if err != nil {
+				return 0, false
+			}
+
+			return int64(userID), true
+		}
+
+		loadUserIntoContext := func(userID int64) {
+			db := models.New(api.UserDB())
+			user, err := db.GetUserByID(req.Context(), userID)
+			if err != nil {
+				if err != sql.ErrNoRows {
+					respondWithInternalError(w, err)
+				}
+				return
+			}
+
+			data, err := db.GetUserData(req.Context(), userID)
+			if err != nil {
+				if err != sql.ErrNoRows {
+					respondWithInternalError(w, err)
+				}
+				return
+			}
+
+			req = req.WithContext(context.WithValue(req.Context(), userKey, &userContextValue{
+				User: user,
+				Data: data,
+			}))
+		}
+
+		if id, ok := userIDFromToken(); ok {
+			loadUserIntoContext(id)
+		}
 
 		next.ServeHTTP(w, req)
 	})
--- api/routes/routes.go
+++ api/routes/routes.go
@@ -9,20 +9,24 @@ import (
 
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/posts", PostsHandler)
+	r.Use(RecoverMiddleware, LoadUserMiddleware)
 	r.HandleFunc("/tags", TagsHandler)
 	r.HandleFunc("/tagsearch", TagSearchHandler)
 	r.HandleFunc("/settings/import", SettingImportHandler).Methods("POST")
 	r.HandleFunc("/settings/export", SettingExportHandler).Methods("POST")
 	r.HandleFunc("/hosts", CDNHostHandler)
-	r.HandleFunc("/register", RegisterHandler).Methods("POST")
-	r.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/version", func(w http.ResponseWriter, req *http.Request) {
 		type versionResponse struct {
 			Version string `json:"version"`
 		}
 		respondJson(w, http.StatusOK, versionResponse{Version: api.AppVersion})
 	})
-	r.Use(RecoverMiddleware)
+	r.HandleFunc("/login", LoginHandler).Methods("POST")
+	r.HandleFunc("/register", RegisterHandler).Methods("POST")
+
+	authRouter := r.NewRoute().Subrouter()
+	authRouter.Use(LoadUserMiddleware)
+	authRouter.HandleFunc("/posts", PostsHandler)
 
 	return r
 }
