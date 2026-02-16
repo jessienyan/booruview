89cf614600df8ff871527080ade53e64611613e0
jessienyan <jessienyan@noreply.codeberg.org>
2026-02-16 17:05:32 -0500

    Fix typo

--- client/src/views/LandingView.vue
+++ client/src/views/LandingView.vue
@@ -1,6 +1,7 @@
-<script type="ts" setup>
+<script lang="ts" setup>
 import NoResults from "@/components/NoResults.vue";
 </script>
+
 <template>
     <NoResults>
         Images aren't loading at the moment, working on a fix
