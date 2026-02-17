06f947b9a4423c8c1f617c0fa48dbdab55888559
jessienyan <jessienyan@noreply.codeberg.org>
2026-02-16 21:33:47 -0500

    Fix caddy failing to start if USE_MEDIA_PROXY=0

--- caddy/generate_caddyfile.sh
+++ caddy/generate_caddyfile.sh
@@ -6,9 +6,13 @@
 #
 # This script lives in /etc/caddy. Component files are in /etc/caddy/components
 
+set -euo pipefail
+
 cd ${0%/*}
 COMPONENT_FILES=components/
 
 cp $COMPONENT_FILES/Caddyfile .
 [[ $USE_GRAFANA = 1 ]] && cat < $COMPONENT_FILES/Caddyfile.grafana >> Caddyfile
 [[ $USE_MEDIA_PROXY = 1 ]] && cat < $COMPONENT_FILES/Caddyfile.proxy >> Caddyfile
+
+exit 0
