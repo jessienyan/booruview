19da25bb2b7f1a84e719cda7cf56e615aa761d94
jessienyan <jessienyan@noreply.codeberg.org>
2026-02-26 17:29:05 -0500

    Update account FAQ

--- client/src/components/sidebar/footer/account/FAQ.vue
+++ client/src/components/sidebar/footer/account/FAQ.vue
@@ -4,11 +4,54 @@ import Collapsable from "@/components/Collapsable.vue";
 
 <template>
     <div class="faq">
-        <Collapsable text="What does having an account do?" :button="false">
+        <Collapsable text="What is an account?" :button="false">
             <div class="collapsable">
                 <p>
                     Accounts lets you easily sync your data across your devices.
                     They're free, entirely optional, and no email required.
+                </p>
+                <p>
+                    Accounts store your favorite posts, favorite tags,
+                    blacklist, and search history.
+                </p>
+                <p>
+                    Things like your layout settings are not synced, however, in
+                    case you want different settings for different devices.
+                </p>
+            </div>
+        </Collapsable>
+
+        <Collapsable
+            text="Can I sync up my device with my account?"
+            :button="false"
+        >
+            <div class="collapsable">
+                <p>
+                    Yes. Login to the device that has your existing favorites,
+                    then click the <strong>Upload Data</strong> button in the
+                    account tab. This will merge your data from your device and
+                    your account together.
+                </p>
+            </div>
+        </Collapsable>
+
+        <Collapsable
+            text="Why do my favorites disappear when I login?"
+            :button="false"
+        >
+            <div class="collapsable">
+                <p>
+                    When you're logged out, you're seeing the data that's saved
+                    in your browser.
+                </p>
+                <p>
+                    When you login, you're seeing the data that's saved in your
+                    account.
+                </p>
+                <p>
+                    If you want to see both, you'll need to sync them together.
+                    Check the answer for "Can I sync up my device with my
+                    account?"
                 </p>
             </div>
         </Collapsable>
@@ -24,7 +67,8 @@ import Collapsable from "@/components/Collapsable.vue";
             </div>
         </Collapsable>
 
-        <Collapsable text="I forgot my password." :button="false">
+        <!--
+        <Collapsable text="Help, I forgot my password!" :button="false">
             <div class="collapsable">
                 <p>
                     While you can't reset your password, I can try and help you
@@ -45,10 +89,11 @@ import Collapsable from "@/components/Collapsable.vue";
                 </ul>
             </div>
         </Collapsable>
+        -->
 
         <Collapsable text="Will accounts ever be required?" :button="false">
             <div class="collapsable">
-                <p>No.</p>
+                <p>No, Booruview is free forever.</p>
             </div>
         </Collapsable>
     </div>
