311f494a406f03e158bddbb21cdb4590ad96ebc9
jessienyan <jessienyan@noreply.codeberg.org>
2026-03-14 23:04:53 -0400

    Add custom goose CLI with embedded migrations

--- api/cmd/goose/main.go
+++ api/cmd/goose/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"os"
 	"path"
 
 	api "codeberg.org/jessienyan/booruview"
+	"codeberg.org/jessienyan/booruview/database/migrations"
+	"github.com/pressly/goose/v3"
 	"github.com/rs/zerolog/log"
-"github.com/pressly/goose/v3"
 )
 
 func main() {
 	api.InitLogging()
 	api.LoadDatabaseEnv()
 
+	flags := flag.NewFlagSet("goose", flag.ExitOnError)
+	flags.Parse(os.Args[1:])
+
+	args := flags.Args()
+	if len(args) < 1 {
+		log.Fatal().Msg("usage: goose <command> [arguments]")
+	}
+
+	// Ensure the path to the database file exists
 	dirPath := path.Dir(api.DatabasePath)
 	if err := os.MkdirAll(dirPath, 0644); err != nil {
 		log.Fatal().Msgf("failed to create path %s: %v", dirPath, err)
 	}
 
-	_, err := os.Stat(api.DatabasePath)
-	if os.IsNotExist(err) {
-		log.Info().Msgf("creating new db: %s", api.DatabasePath)
-		if err := api.InitUserDatabase(); err != nil {
-			log.Fatal().Msgf("error creating db: %v", err)
-		}
-		api.UserDB().Ping()
-	} else if err != nil {
-		log.Fatal().Msgf("error checking for db: %v", err)
+	if err := api.InitUserDatabase(); err != nil {
+		log.Fatal().Msgf("failed to open db: %v", err)
+	}
+	db := api.UserDB()
+
+	// Use the embedded filesystem for the migrations
+	goose.SetBaseFS(migrations.FS)
+
+	command := args[0]
+	args = args[1:]
+	if err := goose.RunContext(context.Background(), command, db, "", args...); err != nil {
+		log.Fatal().Msgf("goose %v: %v", command, err)
 	}
 }
