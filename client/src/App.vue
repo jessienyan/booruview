6b20835d30b087f595eb9bd871c67bf4d606e484
jessienyan <jessienyan@noreply.codeberg.org>
2026-03-17 12:42:51 -0400

    Tweak banner text

--- client/src/App.vue
+++ client/src/App.vue
@@ -50,10 +50,11 @@ const hasConsented = computed(() => {
                 dataUpdate400IssueBanner.show.value
             "
         >
-            We're aware of a bug affecting a small number of users that's
-            preventing them from saving favorites. If this is happening to you,
-            click 'Download Data' in the account tab and logout. We'll post an
-            update once the issue is resolved. Sorry for the inconvenience!
+            <span class="bi bi-exclamation-circle"></span> A small number of
+            users are having issues saving favorites while logged in. If you're
+            affected by this, click <strong>Download Data</strong> in the
+            <strong>account</strong> tab, then logout. We'll post an update once
+            the issue is resolved.
             <a
                 href="#"
                 class="banner-close"
