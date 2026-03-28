7cbedde6c6cd6b1066d1efa6d384f3bcd9551123
jessienyan <jessienyan@noreply.codeberg.org>
2026-03-28 16:37:21 -0400

    Add news

--- client/src/news.tsx
+++ client/src/news.tsx
@@ -10,17 +10,27 @@ type Update = {
 // NOTE: add new updates to the top of the list so it's sorted by most recent
 const updates: Update[] = [
 	{
+		date: new Date("2026-03-28T20:30:16+00:00"),
+		title: "improvements for slow connections",
+		component: (
+			<>
+			<p>Fixed an issue on slower connections where favorites were sometimes not being saved.
+				The site uses less bandwidth and should be a bit more responsive, too.</p>
+			</>
+		)
+	},
+	{
 		date: new Date("2026-03-25T01:27:33+00:00"),
 		title: "please login again",
 		component: (
 			<>
 			<p>I've fixed a couple bugs affecting logged in users:</p>
-				<ul>
-			<li>searches sometimes wouldn't use your blacklist</li>
-			<li>the page gets stuck or takes a while to load</li>
+			<ul>
+				<li>searches sometimes wouldn't use your blacklist</li>
+				<li>the page gets stuck or takes a while to load</li>
 			</ul>
-		<p>Logging in again will fix the issue (<a href="https://codeberg.org/jessienyan/booruview/pulls/39" target="_blank" rel="noopener">#39</a>)</p>
-		<p>Also, the site now uses a login cookie, so consider adding an exception if you have cookies disabled. Thanks ඞ</p>
+			<p>Logging in again will fix the issue (<a href="https://codeberg.org/jessienyan/booruview/pulls/39" target="_blank" rel="noopener">#39</a>)</p>
+			<p>Also, the site now uses a login cookie, so consider adding an exception if you have cookies disabled. Thanks ඞ</p>
 			</>
 		)
 	},
