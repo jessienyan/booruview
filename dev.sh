2a2d7719a07db3665e343cb777afd866262f3156
jessienyan <jessienyan@noreply.codeberg.org>
2026-05-27 21:24:43 -0400

    Fix dev.sh typo

--- dev.sh
+++ dev.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 cd ${0%/*}
 
-export VITE_COMMIT_SHA=$(git rev-parse --short)
+export VITE_COMMIT_SHA=$(git rev-parse --short HEAD)
 export VITE_LAST_COMMIT_DATE=$(git show -s --format=%cs)
 
 docker compose up --build $@
