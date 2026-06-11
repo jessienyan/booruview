975f3f7a753e6af3c21451cf7d7d03a7a2f2607c
jessienyan <jessienyan@noreply.codeberg.org>
2026-06-11 01:20:51 -0400

    Revert "Remove banner"
    This reverts commit 547c64d1c7cc5a54b650aa12cbe754696ea92288.

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,7 @@ import store from "@/store";
 import ContentWarning from "./components/ContentWarning.vue";
 import FullscreenView from "./components/fullscreen-view/FullscreenView.vue";
 import Toast from "./components/Toast.vue";
+import { switchedToThumbsBanner } from "./indicators";
 
 const mainContainer = useTemplateRef("main");
 provide("mainContainer", readonly(mainContainer));
@@ -37,7 +38,6 @@ const hasConsented = computed(() => {
 
 <template>
     <div class="app-outer">
-        <!--
         <div
             class="banner banner-warning"
             v-if="hasConsented && switchedToThumbsBanner.show.value"
@@ -51,7 +51,6 @@ const hasConsented = computed(() => {
                 >close</a
             >
         </div>
-        -->
         <div
             class="app"
             :class="{
